a84fd6e0fcb483d9f51a09310004ebf429d9064c
tchen <tchen0584@gmail.com>
2020-09-13 16:07:24 -0400

    I forgot to use VCS. I'm halfway done already. Why, has I ever.

---
+++ basics/__init__.py
@@ -0,0 +1,13 @@
+"""
+Raspbot Remote Control Application (Raspbot RCA, Raspbot RCA-G), v1.2
+basics.py module, contains basic application functions such as exiting client software, multiprocessing, and editing configs.
+Made by perpetualCreations
+
+Client version of basics module.
+"""
+
+print("[INFO]: Initiating basics module...")
+
+from basics import basics, objects, process
+
+print("[INFO]: Initiation of basics complete!")
---
+++ basics/objects.py
@@ -0,0 +1,25 @@
+"""
+Raspbot Remote Control Application (Raspbot RCA, Raspbot RCA-G), v1.2
+basics module, contains basic application functions such as exiting client software, multiprocessing, and editing configs.
+Made by perpetualCreations
+
+Contains objects for module, including any package imports. Interact with these objects through basics.objects.
+"""
+
+try:
+    import multiprocessing, sys, configparser
+    from time import gmtime, strftime
+    from tkinter import messagebox
+    from basics import basics
+except ImportError as ImportErrorMessage:
+    print("[FAIL]: Import failed!")
+    print(ImportErrorMessage)
+    basics.exit(1)
+except ImportWarning as ImportWarningMessage:
+    print("[FAIL]: Imports raised warnings.")
+    print(ImportWarningMessage)
+    basics.exit(1)
+pass
+
+log_file_handle = None # overwritten by basics module's log_init()
+origin_stdout = None # overwritten by basics module's log_init()
